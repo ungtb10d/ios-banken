0e098ed49587e67ab91816205c413dd00302b6b1
Øyvind Tjervaag <oyvind.tjervaag@sbanken.no>
2020-11-12 07:53:40 +0100

    Possible to set expiry date for token

--- SbankenClient/Models/AccessToken.swift
+++ SbankenClient/Models/AccessToken.swift
@@ -12,15 +12,24 @@ public class AccessToken: Codable {
     public var accessToken: String
     public var expiresIn: Int
     public var tokenType: String
+    public var expiresAt: Date?
     
     lazy var expiryDate: Date = {
-        Calendar.current.date(byAdding: .second, value: expiresIn, to: Date())!
+        expiresAt ?? Calendar.current.date(byAdding: .second, value: expiresIn, to: Date())!
     }()
     
-    public init(_ accessToken: String, expiresIn: Int, tokenType: String) {
+    public init(_ accessToken: String, expiresIn: Int, tokenType: String, expiresAt: Date?) {
         self.accessToken = accessToken
         self.expiresIn = expiresIn
         self.tokenType = tokenType
+        self.expiresAt = expiresAt
+    }
+    
+    public func hasExpired() -> Bool {
+        guard let expiresAt = self.expiresAt else {
+            return false
+        }
+        return Date() < expiresAt ? true : false
     }
     
     enum CodingKeys: String, CodingKey {
